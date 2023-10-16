32e0e3fb7607922ef1170adf50bdb4164bf41a9e
Yeider <yeiderjp12@gmail.com>
2023-10-16 15:58:23 -0500

    Creando preparacion para despliegue en produccion

--- src/components/Peticiones/peticiones.js
+++ src/components/Peticiones/peticiones.js
@@ -1,5 +1,5 @@
 export async function Peticiones(food, page){
-  const data = await fetch(`http://localhost:3000/main?food=${food}&page=${page}`)
+  const data = await fetch(`https://food-api-7g6u.onrender.com/main?food=${food}&page=${page}`)
   const dataJson = await data.json()
   return dataJson
 }
